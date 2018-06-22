8904915afd71bba2c831dbafdd795e4d4373994a
nanmu2018 <1213043499@qq.com>
2018-06-22 21:06:26 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+  <!--  显示的是当前路由地址显示的内容 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
